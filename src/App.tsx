2a3649174af593c35d1d4fea77d627190230239c
Sota Hirose <za446131@gmail.com>
2023-10-16 00:16:20 +0900

    format

--- src/App.tsx
+++ src/App.tsx
@@ -72,11 +72,11 @@ export const App: React.FC = () => {
                 id="files"
                 type="file"
                 className="block w-full cursor-pointer text-sm
-              text-slate-500 file:mr-4 file:rounded-full file:border-0
-              file:bg-violet-50 file:px-4
-              file:py-2 file:text-sm
-              file:font-semibold file:text-violet-700
-              hover:file:bg-violet-100"
+                  text-slate-500 file:mr-4 file:rounded-full file:border-0
+                  file:bg-violet-50 file:px-4
+                  file:py-2 file:text-sm
+                  file:font-semibold file:text-violet-700
+                hover:file:bg-violet-100"
                 multiple
                 accept="image/*"
                 onChange={handleFileChange}
